bb55e92fea4807a3fdf1b830b0614e4b4ce8da7b
tamio0800 <tamio0800@users.noreply.github.com>
2026-08-19 02:58:43 +0800

    chore: update lasay to 1.2.1

--- Casks/lasay.rb
+++ Casks/lasay.rb
@@ -1,8 +1,8 @@
 cask "lasay" do
-  version "1.2.0"
-  sha256 "7cea16b680a9d63d0593bda4f0c42f88bde69048020d6d22176df6b2e411c5f2"
+  version "1.2.1"
+  sha256 "9c0ed43743b48b0befa1c806e4c9f15ac1e5b0ce601287ddce5856e3835e3d47"
 
-  url "https://github.com/tamio0800/LaSay/releases/download/v#{version}/LaSay-v#{version}%2B5.dmg"
+  url "https://github.com/tamio0800/LaSay/releases/download/v#{version}/LaSay-v#{version}%2B6.dmg"
   name "LaSay"
   desc "Privacy-first macOS voice-to-text app"
   homepage "https://github.com/tamio0800/LaSay"
