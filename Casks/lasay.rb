1b7620cf90c041df484960190bf5fdfd57ba6328
tamio0800 <tamio0800@users.noreply.github.com>
2026-08-19 22:57:57 +0800

    lasay 1.2.2

--- Casks/lasay.rb
+++ Casks/lasay.rb
@@ -1,8 +1,8 @@
 cask "lasay" do
-  version "1.2.1"
-  sha256 "9c0ed43743b48b0befa1c806e4c9f15ac1e5b0ce601287ddce5856e3835e3d47"
+  version "1.2.2"
+  sha256 "be45e15bfb49cc52c090f61eb585a31269c942e3daafa83bc61079c9a5628698"
 
-  url "https://github.com/tamio0800/LaSay/releases/download/v#{version}/LaSay-v#{version}%2B6.dmg"
+  url "https://github.com/tamio0800/LaSay/releases/download/v#{version}/LaSay-v#{version}%2B7.dmg"
   name "LaSay"
   desc "Privacy-first macOS voice-to-text app"
   homepage "https://github.com/tamio0800/LaSay"
